6695697fa83117cd37e05ae821ac780c10306e3d
Rajiv Bose <rbose85@users.noreply.github.com>
2016-03-31 18:31:49 +0100

    create "field of stars" using range() operator func

--- scripts.js
+++ scripts.js
@@ -5,3 +5,13 @@ document.body.appendChild(canvas);
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const SPEED = 40;
+const STAR_NUMBER = 250;
+
+var StarStream = Rx.Observable.range(1, STAR_NUMBER)
+    .map(() => ({
+      x: parseInt(Math.random() * canvas.width),
+      y: parseInt(Math.random() * canvas.height),
+      size: Math.random() * 3 + 1
+    }));
