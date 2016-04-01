34927ca15eefb62d50b32310a0ef798d08fe9373
Rajiv Bose <rbose85@users.noreply.github.com>
2016-04-01 07:18:41 +0100

    refactor how star data is adjusted per interval

--- scripts.js
+++ scripts.js
@@ -19,19 +19,20 @@ const paintStars = stars => {
 const SPEED = 40;
 const STAR_NUMBER = 250;
 
-var StarStream = Rx.Observable.range(1, STAR_NUMBER)
-    .map(() => ({
-      x: parseInt(Math.random() * canvas.width),
-      y: parseInt(Math.random() * canvas.height),
-      size: Math.random() * 3 + 1
-    }))
+const getStar = () => ({
+  x: parseInt(Math.random() * canvas.width),
+  y: parseInt(Math.random() * canvas.height),
+  size: Math.random() * 3 + 1
+});
+
+Rx.Observable.range(1, STAR_NUMBER)
+    .map(() => getStar())
     .toArray()
-    .flatMap(stars => Rx.Observable
-        .interval(SPEED)
+    .flatMap(stars => Rx.Observable.interval(SPEED)
         .map(() => {
           stars.forEach(star => {
             if (star.y >= canvas.height) {
-              star.y = 0; // reset star top top of screen
+              Object.assign(star, getStar(), { y: 0 });
             }
 
             star.y += 3;  // move star
